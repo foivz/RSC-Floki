ae4336998d19dc56e84a7133bbf9f11fde7bb472
Davor Obilinovic <obee88@gmail.com>
2014-11-22 22:20:01 +0100

    push user

--- app/views/REST.py
+++ app/views/REST.py
@@ -1,9 +1,28 @@
-from flask import jsonify
+from flask import jsonify, request
 from app import app
+from app.DAO import user as userClass, mongo
+from app.DAO.user import User
 
 __author__ = 'Davor Obilinovic'
 
 
+@app.route("/REST/profile")
+def rest_profile():
+    usersCur = mongo.UserDocument.find(request.args)
+    results = []
+    for userObj in usersCur:
+        user = User(userObj)
+        results.append(user.getProfileJson())
+    return jsonify(status="OK",results=results)
+
+@app.route("/REST/login", methods=["POST"])
+def login_donor():
+    data = request.args
+    username, password = data["username"],data["password"]
+    userClass.get_by_username()
+    pass
+
 @app.route("/REST/test/")
 def rest_test():
     return jsonify(status="OK",info={"disi":"frane"})
+
