b58deaed869daab49f8c7e4e0166edd8e8cfe5a3
Davor Obilinovic <obee88@gmail.com>
2014-11-22 16:33:18 +0100

    reg requests

--- app/views/admin.py
+++ app/views/admin.py
@@ -1,14 +1,19 @@
 from flask import render_template
-from flask.ext.login import current_user
+from flask.ext.login import current_user, login_required
 from app import app
 from app.core import Functions
 
 __author__ = 'Davor Obilinovic'
 
-
-@app.route("/super/admin", methods=["GET"])
-def super_admin():
+@login_required
+@app.route("/super/admin/registrationRequests", methods=["GET"])
+def registration_requests():
+    worker_registation_requests = Functions.pull_registration_requests("worker")
+    institution_registation_requests = Functions.pull_registration_requests("institution")
+    total_requests_num = len(worker_registation_requests) + len(institution_registation_requests)
     return render_template("admin/registrationRequests.html",
-                           worker_registation_requests = Functions.pull_registration_requests("worker"),
-                           institution_registation_requests = Functions.pull_registration_requests("institution"),
+                           worker_registation_requests = worker_registation_requests,
+                           institution_registation_requests = institution_registation_requests,
+                           total_requests_num = total_requests_num,
                            user=current_user)
+
