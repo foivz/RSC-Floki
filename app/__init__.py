a9388a30791ce6a6f76c4c46ee72843af3185f16
Davor Obilinovic <obee88@gmail.com>
2014-11-22 17:19:16 +0100

    registration

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ login_manager.login_view = "/login"
 login_manager.session_protection = "strong"
 
 
-from views import login, pages, admin, callbacks
+from views import login, pages, admin, callbacks, system
 
 @login_manager.user_loader
 def load_user(userId):
