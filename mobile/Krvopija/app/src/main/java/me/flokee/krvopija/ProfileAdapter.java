4f61dfcd6807a34bf7d1e541ef28adbd867b3390
Nino Uzelac <uzelac.nino@gmail.com>
2014-11-22 21:21:39 +0100

    update ListView

--- mobile/Krvopija/app/src/main/java/me/flokee/krvopija/ProfileAdapter.java
+++ mobile/Krvopija/app/src/main/java/me/flokee/krvopija/ProfileAdapter.java
@@ -1,15 +1,19 @@
 package me.flokee.krvopija;
 
+import android.app.Activity;
 import android.content.Context;
 import android.provider.ContactsContract;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
-public class ProfileAdapter extends RecyclerView.Adapter<ProfileItemRowHolder> {
+public class ProfileAdapter extends BaseAdapter {
 
     private List<ProfileItem> profileItems;
     private Context mContext;
@@ -18,24 +22,42 @@ public class ProfileAdapter extends RecyclerView.Adapter<ProfileItemRowHolder> {
         this.mContext = context;
         this.profileItems = profileItems;
     }
-
     @Override
-    public ProfileItemRowHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.profile_list_item, null);
-        ProfileItemRowHolder pirh = new ProfileItemRowHolder(v);
-        return pirh;
+    public int getCount() {
+        return profileItems.size();
     }
 
     @Override
-    public void onBindViewHolder(ProfileItemRowHolder feedListRowHolder, int i) {
-        ProfileItem feedItem = profileItems.get(i);
-
-        feedListRowHolder.text.setText(feedItem.text);
-        feedListRowHolder.thumbnail.setImageResource(feedItem.icon);
+    public Object getItem(int i) {
+        return profileItems.get(i);
     }
 
     @Override
-    public int getItemCount() {
-        return (null != profileItems ? profileItems.size() : 0);
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        if(view == null) {
+            LayoutInflater mInflater = (LayoutInflater) mContext
+                    .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            view = mInflater.inflate(R.layout.profile_list_item, null);
+        }
+
+        ImageView icon = (ImageView) view.findViewById(R.id.thumbnail);
+        TextView text = (TextView) view.findViewById(R.id.text);
+
+        ProfileItem profileItem = (ProfileItem) getItem(i);
+        icon.setImageResource(profileItem.icon);
+        text.setText(profileItem.text);
+
+        return view;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return false;
     }
 }
